53c333d42186d23165c27eb491f782a190384b9f
hubrigant <jonhatesspam+github@gmail.com>
2019-02-19 13:52:45 -0600

    fixing clone command, which was missing a . at the end

--- bin/startgit.sh
+++ bin/startgit.sh
@@ -4,6 +4,6 @@
 # git remote add -f origin https://github.com/hubrigant/dotfiles
 # git remote set-head origin -a
 # git checkout master
-git clone --separate-git-dir=.dotfiles.git https://github.com/hubrigant/dotfiles
+git clone --separate-git-dir=.dotfiles.git https://github.com/hubrigant/dotfiles .
 rm .git
 echo '*' > .dotfiles.git/info/exclude
