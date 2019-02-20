784f576e9aaf28dbb59cc02dc284a6eb4a7116e0
hubrigant <34658736+hubrigant@users.noreply.github.com>
2019-02-19 21:44:31 -0600

    Update startgit.sh

--- bin/startgit.sh
+++ bin/startgit.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git init --separate-git .
-git remote add origin https://github.com/hubrigant/dotfiles
+git remote add -f origin https://github.com/hubrigant/dotfiles
 git remote set-head origin -a
 git checkout master
 git clone --separate-git-dir=.dotfiles.git https://github.com/hubrigant/dotfiles .
