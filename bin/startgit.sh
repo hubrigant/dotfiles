c88b44ee00496874ade722e2d073d49c3d5d7042
hubrigant <34658736+hubrigant@users.noreply.github.com>
2019-02-19 21:42:32 -0600

    Update startgit.sh

--- bin/startgit.sh
+++ bin/startgit.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# git init --separate-git .
-# git remote add -f origin https://github.com/hubrigant/dotfiles
-# git remote set-head origin -a
-# git checkout master
+git init --separate-git .
+git remote add -f origin https://github.com/hubrigant/dotfiles
+git remote set-head origin -a
+git checkout master
 git clone -f --separate-git-dir=.dotfiles.git https://github.com/hubrigant/dotfiles .
 rm .git
 echo '*' > .dotfiles.git/info/exclude
