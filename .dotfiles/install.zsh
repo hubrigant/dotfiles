304f9bf72bf103696d56eeabd6c36f585e672611
Jon Williams <williamsjon@johndeere.com>
2019-04-10 13:49:07 -0500

    minor modification to install script since I don't use sublime

--- .dotfiles/install.zsh
+++ .dotfiles/install.zsh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+### Copied from https://www.outcoldman.com/en/archive/2015/09/13/keep-your-sh-together/
+
 ## Change shell for current user to zsh
 if [ ! "$SHELL" = "/bin/zsh" ]; then
 chsh -s /bin/zsh
@@ -20,10 +22,13 @@ ln ~/.dotfiles/dots/home/vimrc                   ~/.vimrc
 ln ~/.dotfiles/dots/home/zshrc                   ~/.zshrc
 
 # Do special to sync sublime settings on OS X
-if [[ "$OSTYPE" =~ "darwin" ]]; then
-    rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-    ln -s ~/.dotfiles/settings/SublimeText/User      ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-fi
+# Since I don't use Sublime at the moment, commenting this out
+# I'll re-enable it later if Sublime gets used more
+#
+#if [[ "$OSTYPE" =~ "darwin" ]]; then
+#    rm ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+#    ln -s ~/.dotfiles/settings/SublimeText/User      ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+#fi
 
 
 # install powerline fonts
