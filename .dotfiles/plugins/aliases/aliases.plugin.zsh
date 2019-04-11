6be56941abba9528173b17cf740cc52dcf773831
Jon Williams <williamsjon@johndeere.com>
2019-04-11 11:03:15 -0500

    disabling aliases referencing app I don't use

--- .dotfiles/plugins/aliases/aliases.plugin.zsh
+++ .dotfiles/plugins/aliases/aliases.plugin.zsh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
-alias zshconfig="code ~/.zshrc"
-alias ohmyzsh="code ~/.oh-my-zsh"
-alias dotfiles="git --git-dir=$HOME/.dotfiles.git"
+# alias zshconfig="code ~/.zshrc"
+# alias ohmyzsh="code ~/.oh-my-zsh"
+# alias dotfiles="git --git-dir=$HOME/.dotfiles.git"
 alias vi="vim"
 alias txst="tmuxinator start traffic"
