3274ed7cc493f0606e35a1fa440f6c9401c53658
Jonathan Williams <jon@JonMac-3.local>
2019-04-11 17:50:32 -0500

    removing old initialization script that's no longer needed

--- bin/startgit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git init --separate-git-dir=.dotfiles.git .
-git remote add -f origin https://github.com/hubrigant/dotfiles
-git remote set-head origin -a
-git checkout master
-git clone --separate-git-dir=.dotfiles.git https://github.com/hubrigant/dotfiles .
-rm .git
-echo '*' > .dotfiles.git/info/exclude
