d17c4794e062d0184504aa6f1d244b247eedf905
Jon Williams <williamsjon@johndeere.com>
2019-05-08 14:42:53 -0500

    adjustments to get history plugin aliases working again

--- plugins/aliases/aliases.plugin.zsh
+++ plugins/aliases/aliases.plugin.zsh
@@ -4,3 +4,14 @@ alias vi="vim"
 alias txst="tmuxinator start traffic"
 alias jd='j d'
 alias ls='ls -FG'
+
+# history aliases copied from the oh-my-zsh history plugin
+# added here because the other plugin isn't working
+alias h='history'
+
+function hs
+{
+    history | grep $*
+}
+
+alias hsi='hs -i'
