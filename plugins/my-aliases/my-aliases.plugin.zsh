533720046152cec2a266d334f7f047d4d7b0f644
Jon Williams <williamsjon@johndeere.com>
2019-05-28 15:01:25 -0500

    some more alias work

--- plugins/my-aliases/my-aliases.plugin.zsh
+++ plugins/my-aliases/my-aliases.plugin.zsh
@@ -1,9 +1,12 @@
 #!/usr/bin/env zsh
 
+# basic conveniences
 alias vi="vim"
 alias txst="tmuxinator start traffic"
 alias jd='j d'
 alias ls='ls -FG'
+alias cd-='cd -'
+alias alias-reload='source ~/.dotfiles/plugins/my-aliases/my-aliases.plugin.zsh'
 
 # history aliases copied from the oh-my-zsh history plugin
 # added here because the other plugin isn't working
@@ -27,5 +30,3 @@ alias all-commit='git commit -a -m'
 alias gs='git status'
 alias gp='git push'
 alias ga='git add'
-
-alias alias-reload='source ~/.dotfiles/plugins/my-aliases/my-aliases.plugin.zsh'
