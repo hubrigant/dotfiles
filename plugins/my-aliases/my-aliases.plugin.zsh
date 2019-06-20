dba716114decb94fcf5e9b5c38b73b07366d9a1a
Jon Williams <williamsjon@johndeere.com>
2019-06-20 09:58:32 -0500

    more convenience aliases:

--- plugins/my-aliases/my-aliases.plugin.zsh
+++ plugins/my-aliases/my-aliases.plugin.zsh
@@ -1,23 +1,29 @@
 #!/usr/bin/env zsh
 
 # basic conveniences
-alias vi='vim'
-alias jd='j d'
-alias ls='ls -FG'
-alias cd-='cd -'
-alias alias-reload='source ~/.dotfiles/plugins/my-aliases/my-aliases.plugin.zsh'
-alias alias-edit='vi ~/.dotfiles/plugins/my-aliases/my-aliases.plugin.zsh'
 alias ale='vi ~/.dotfiles/plugins/my-aliases/my-aliases.plugin.zsh'
 alias alr='source ~/.dotfiles/plugins/my-aliases/my-aliases.plugin.zsh'
 alias bue='vi ~/.bundles.vim'
 alias bur='/usr/local/bin/vim +PluginInstall +qall'
-alias buc='/usr/local/bin/vim +PluginClean +qall'
-alias reinstall='~/.dotfiles/scripts/install_dotfiles.sh'
-alias install-edit='vi ~/.dotfiles/scripts/install_dotfiles.sh'
+alias cd-='cd -'
 alias ine='vi ~/.dotfiles/scripts/install_dotfiles.sh'
 alias inr='~/.dotfiles/scripts/install_dotfiles.sh'
+alias jd='j d'
 alias jl='jupyter-lab'
+alias ls='ls -FG'
 alias top='htop'
+alias vi='vim'
+alias vie='vi ~/.dotfiles/config_masters/vimrc'
+alias zre='vi ~/.dotfiles/config_masters/zshrc'
+alias zrr='source ~/.zshrc'
+alias zee='vi ~/.dotfiles/config_masters/zshenv'
+alias zer='source ~/.zshenv'
+
+function buc(){
+    rm -rf ~/.vim/bundle
+    vundle-init
+    /usr/local/bin/vim +PluginInstall +qall
+}
 
 # history aliases copied from the oh-my-zsh history plugin
 # added here because the other plugin isn't working
@@ -41,7 +47,7 @@ alias all-commit='git commit -a -m'
 alias ga='git add'
 alias gca='git commit -a -m'
 alias gcm='git commit -m'
-alias gd='git diff'
+alias gd='git difftool'
 alias gp='git push'
 alias gs='git status'
 alias gpu='git pull'
