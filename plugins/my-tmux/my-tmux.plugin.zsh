36baa9cf19604d91198fde3fc2c7ef76b657d615
Jon Williams <jonhatesspam+gitrepo@gmail.com>
2020-02-13 10:45:24 -0600

    fixing a path in my own tmux plugin

--- plugins/my-tmux/my-tmux.plugin.zsh
+++ plugins/my-tmux/my-tmux.plugin.zsh
@@ -3,5 +3,5 @@
 # This takes control, so load last
 # Load only when we are default user (not root) and not in ssh
 if [[ ${DEFAULT_USER} == ${USER} ]] && [[ -z ${SSH_CLIENT} ]]; then
-    source $ZSH/plugins/tmux/tmux.plugin.zsh
+    source ${DOTFILES}/plugins/tmux/tmux.plugin.zsh
 fi
