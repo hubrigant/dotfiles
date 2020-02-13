c81b4bb22227e35a96edf64e2c8c5d7d6323f992
Jon Williams <jonhatesspam+gitrepo@gmail.com>
2020-02-13 10:44:37 -0600

    removing now-unnecessary debugging statements

--- config_masters/theming.zsh
+++ config_masters/theming.zsh
@@ -1,13 +1,10 @@
 TRY_THEME=p10k
 if [ -n "${TRY_THEME+1}" ]; then
-    echo "I want to try ${TRY_THEME}"
     if [ -f ${HOME}/.${TRY_THEME}.zsh ]; then
-        echo "Trying ${TRY_THEME}"
         source ${HOME}/.${TRY_THEME}.zsh
     else;
         echo "TRY_THEME was set, but the RC file ${HOME}/.${TRY_THEME}.zsh doesn't exist"
     fi
 else;
-    echo "I don't want to try a different theme, I got ${TRY_THEME} as my variable"
     source ${HOME}/.p9k.zsh
 fi
