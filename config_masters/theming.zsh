ad6bc158c3b047678d33f1978d0f199f86aca16f
Jon Williams <jonhatesspam+gitrepo@gmail.com>
2020-03-14 10:27:49 -0500

    setting one of the echo statements to display only when DEBUG is set

--- config_masters/theming.zsh
+++ config_masters/theming.zsh
@@ -1,7 +1,9 @@
 TRY_THEME="p10k"
 if [ -n "${TRY_THEME+1}" ]; then
     if [ -f ${HOME}/.${TRY_THEME}.zsh ]; then
-        echo "Using the ${TRY_THEME} theme."
+        if ${DEBUG}; then
+            echo "Using the ${TRY_THEME} theme."
+        fi
         source ${HOME}/.${TRY_THEME}.zsh
     else;
         echo "TRY_THEME was set, but the RC file ${HOME}/.${TRY_THEME}.zsh doesn't exist"
